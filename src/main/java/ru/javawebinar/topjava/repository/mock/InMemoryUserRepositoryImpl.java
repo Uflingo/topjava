c5e2386e2e81f5f3f934891f2f6f976576fa0af3
uflingo <ufo_2@mail.ru>
2017-07-26 02:43:42 +0300

    Implemented inmemory user repoitory

---
+++ src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryUserRepositoryImpl.java
@@ -0,0 +1,64 @@
+package ru.javawebinar.topjava.repository.mock;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.repository.UserRepository;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Repository
+public class InMemoryUserRepositoryImpl implements UserRepository {
+    private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepositoryImpl.class);
+
+    private Map<Integer, User> repository = new ConcurrentHashMap<>();
+    private AtomicInteger counter = new AtomicInteger(0);
+
+    @Override
+    public boolean delete(int id) {
+        log.info("delete {}", id);
+        if (repository.containsKey(id)){
+            repository.remove(id);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public User save(User user) {
+        log.info("save {}", user);
+        if (user.isNew())
+            user.setId(counter.incrementAndGet());
+        repository.put(user.getId(),user);
+        return user;
+    }
+
+    @Override
+    public User get(int id) {
+        log.info("get {}", id);
+        return repository.get(id);
+    }
+
+    @Override
+    public List<User> getAll() {
+        log.info("getAll");
+        List<User> list = new ArrayList<>();
+        list.addAll(repository.values());
+        list.stream().sorted(new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        });
+        return list;
+    }
+
+    @Override
+    public User getByEmail(String email) {
+        log.info("getByEmail {}", email);
+        return repository.values().stream().filter(u -> u.getEmail().equals(email)).findFirst().get();
+    }
+}
